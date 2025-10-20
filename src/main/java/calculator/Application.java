6ca983585aaed8b55fe2a0626d7539caa58e7a85
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2025-10-20 21:38:16 +0900

    feat(application): api를 활용한 문자열 입력과 계산 결과 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AddString addString = new AddString();
+        StringSplitter stringSplitter = new StringSplitter();
+        IntConverter intConverter = new IntConverter();
+
+        System.out.println("덧셈할 문자열을 입력하세요: ");
+        String inputString = Console.readLine();
+        inputString = inputString.replace("\\n", "\n");
+
+        String[] strings = stringSplitter.split(inputString);
+        int[] numbers = intConverter.intConvert(strings);
+        int result = addString.sum(numbers);
+
+        System.out.println("결과: " + result);
     }
 }
+
