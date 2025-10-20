aefdb8e12eb57dedd8ee9d002c9a6c5a5d1c9120
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2025-10-20 21:41:19 +0900

    feat(AddString): 문자열을 변환한 정수를 덧셈하는 연산

---
+++ src/main/java/calculator/AddString.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class AddString {
+    public int sum(int[] numbers) {
+        int sumResult = 0;
+
+        for (int number : numbers) {
+            sumResult += number;
+        }
+        return sumResult;
+    }
+}
+
